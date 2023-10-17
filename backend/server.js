b3942912be0904b33145649660db4f31bf24cb4b
M.SADHASIVAM <69914346+sadhap@users.noreply.github.com>
2023-10-17 12:08:19 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ process.on('unhandledRejection',(err)=>{
     console.log(`Error:${err.message}`);
     console.log('shutting down the server due to unhandled rejection error');
     server.close(()=>{
-        process.exit(1)
+        Process.exit(1)
     })
 })
 
@@ -20,7 +20,7 @@ process.on('uncaughtException',(err)=>{
     console.log(`Error:${err.message}`);
     console.log('shutting down the server due to uncaughtException  error');
     server.close(()=>{
-        process.exit(1)
+        Process.exit(1)
     })
 })                                                                                                               
 // console.log(a);E:\USER FILES DONT DELETE\Downloads
